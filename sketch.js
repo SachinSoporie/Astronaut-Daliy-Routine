b4508ed842babbd7d84fd448d73ba6e22e691866
SachinSoporie <85801708+SachinSoporie@users.noreply.github.com>
2021-07-16 11:14:43 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,24 +2,25 @@ var bath1, bath2;
 var eat1, eat2;
 var gym1, gym2, gym11, gym12;
 var move, modrink1, drink2;
-var eat1,ve1;
+var eat1, ve1;
 var iss, brush, sleep;
 var edges;
 
 
-function preload(){
-  
-bg = loadImage("iss.png");
-sleep = loadAnimation("sleep.png");
-brush = loadAnimation("brush.png");
-gym = loadAnimation("gym1.png", "gym2.png", "gym11.png", "gym12.png");
-eat = loadAnimation("eat1.png", "eat2.png");
-drink = loadAnimation("drink1.png", "drink2.png");
-bath = loadAnimation("bath1.png", "bath2.png");
-move = loadAnimation("move.png","move1.png");
+function preload() {
+
+  bg = loadImage("iss.png");
+  sleep = loadAnimation("sleep.png");
+  brush = loadAnimation("brush.png");
+  gym = loadAnimation("gym1.png", "gym2.png", "gym11.png", "gym12.png");
+  eat = loadAnimation("eat1.png", "eat2.png");
+  drink = loadAnimation("drink1.png", "drink2.png");
+  bath = loadAnimation("bath1.png", "bath2.png");
+  move = loadAnimation("move.png", "move1.png","move.png","move1.png");
 }
+
 function setup() {
-  createCanvas(800,400);
+  createCanvas(800, 400);
 
   bgg = createSprite(400, 200);
   bgg.addImage(bg);
@@ -34,38 +35,37 @@ function setup() {
 }
 
 function draw() {
+  astronaut.bounceOff(edges);
 
-  if(keyDown("UP_ARROW")){
+  if (keyDown("UP_ARROW")) {
     astronaut.addAnimation("brushing", brush);
     astronaut.changeAnimation("brushing");
     astronaut.y = 200;
     astronaut.velocityX = 0;
     astronaut.velocityY = 0;
   }
-  if(keyDown("DOWN_ARROW")){
+  if (keyDown("DOWN_ARROW")) {
     astronaut.addAnimation("gym", gym);
     astronaut.changeAnimation("gym");
   }
-  if(keyDown("LEFT_ARROW")){
+  if (keyDown("LEFT_ARROW")) {
     astronaut.addAnimation("bath", bath);
     astronaut.changeAnimation("bath");
   }
-  if(keyDown("RIGHT_ARROW")){
+  if (keyDown("RIGHT_ARROW")) {
     astronaut.addAnimation("eat", eat);
     astronaut.changeAnimation("eat");
   }
-  if(keyDown("D")){
+  if (keyDown("D")) {
     astronaut.addAnimation("drink", drink);
     astronaut.changeAnimation("drink");
   }
-  if(keyDown("M")){ 
-  astronaut.addAnimation("moving", move); 
-  astronaut.changeAnimation("moving"); 
-  astronaut.velocityX = 1; 
-  astronaut.velocityY = 1; 
-
-  astronaut.bounceOff(edges);
-}
+  if (keyDown("M")) {
+    astronaut.addAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    astronaut.velocityX = 1;
+    astronaut.velocityY = 1;
+  }
 
   drawSprites();
 }
